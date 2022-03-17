ce9acb4193faa5f18951d60736e3bed3cdffb4b3
Hyojin <dlgywls1130@gmail.net>
2022-03-17 23:38:04 +0900

    html,css 수정사항

--- js/main.js
+++ js/main.js
@@ -16,6 +16,82 @@ const showMenu = (headerToggle, navbarId) =>{
 }
 showMenu('header-toggle','navbar')
 
+//calendar
+
+let date = new Date();
+
+const renderCalender = () => {
+    
+    const viewYear = date.getFullYear();
+    const viewMonth = date.getMonth();
+
+    document.querySelector('.year_month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
+
+    const prevLast = new Date(viewYear, viewMonth, 0);
+    const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+    const PLDate = prevLast.getDate();
+    const PLDay = prevLast.getDay();
+
+    const TLDate = thisLast.getDate();
+    const TLDay = thisLast.getDay();
+
+    const prevDates = [];
+    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+    const nextDates = [];
+
+    if (PLDay !== 6) {
+        for (let i = 0; i < PLDay + 1; i++) {
+            prevDates.unshift(PLDate - i);
+        }
+    }
+
+    for (let i = 1; i < 7 - TLDay; i++){
+        nextDates.push(i);
+    }
+
+    const dates = prevDates.concat(thisDates, nextDates);
+    const firstDateIndex = dates.indexOf(1);
+    const lastDateIndex = dates.lastIndexOf(TLDate);
+
+    dates.forEach((date, i) => {
+        const condition = i >= firstDateIndex && i < lastDateIndex + 1
+                        ? 'this'
+                        : 'other';
+        dates[i] = `<div class="date"><span class=${condition}>${date}</span></div>`;
+    });
+
+    document.querySelector('.dates').innerHTML = dates.join(' ');
+
+    const today = new Date();
+    if(viewMonth === today.getMonth() && viewYear === today.getFullYear()){
+        for ( let date of document.querySelectorAll('.this')) {
+            if (+date.innerText === today.getDate()) {
+                date.classList.add('today');
+                break;
+            }
+        }
+    }
+};
+
+renderCalender();
+
+
+const prevMonth = () => {
+    date.setMonth(date.getMonth() - 1);
+    renderCalender();
+};
+
+const nextMonth = () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalender();
+};
+
+const goToday = () => {
+    date = new Date();
+    renderCalender();
+};
+
 /*==================== LINK ACTIVE ====================*/
 const linkColor = document.querySelectorAll('.nav__link')
 
@@ -50,6 +126,3 @@ adressFilter.addEventListener('click', (e) => {
 });
 
 
-function passwordBtn(){
-    confirm("관리자에게 임시비밀번호를 받겠습니까?");
-  }
